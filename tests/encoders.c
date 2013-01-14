54b8da31a8ea383d3a4d6cc58182e178e7a5cf3d
Nicola Greco <notsecurity@gmail.com>
2013-01-14 21:26:58 +0100

    implemented a test framework

--- tests/encoders.c
+++ tests/encoders.c
@@ -8,28 +8,42 @@
 #include <regex.h>
 #include <assert.h>
 #include "../common.h"
+#include "minunit.h"
+
+int tests_run = 0;
+int tests_fail = 0;
 
 
-static void encodersCmdTest() {
-    assert(0);
-}
-
-static void encodersParseTest() {
-
-    char* elaborated[5];
-    sensors* Sensors;
+static char * encodersParse_test() {
+  /* Scenario */
+    char* elaborated[80];
+    sensors Sensors;
     elaborated[0] = "S";
     elaborated[1] = "MELR";
     elaborated[2] = "123";
     elaborated[3] = "456";
-    encodersParse(elaborated, Sensors);
-    assert(Sensors->encodersL == 123);
-    assert(Sensors->encodersR == 456);
+  /* Function */
+    encodersParse(elaborated, &Sensors);
+  /* Assertion */
+    should("String converted to number for l", Sensors.encodersL == 123);
+    should("String converted to number for r", Sensors.encodersR == 456);
+    return 0;
 }
 
-int main (){
+static char * all_tests() {
+    it("encodersParse", encodersParse_test);
+    return 0;
+}
 
-//    encodersCmdTest();
-    encodersParseTest();
-    
+int main(int argc, char **argv) {
+    char *result = all_tests();
+    if (tests_fail != 0) {
+      printf("\n%d assertion failed, %d in tests\n", tests_fail, tests_run);
+    }
+    else {
+        printf("\nALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+
+    return result != 0;
 }
