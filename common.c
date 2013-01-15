681c4bdd37f9ca5f5d2e7708682bea26efeed72e
andrewgrex <andrewgrex@hotmail.co.uk>
2013-01-15 19:41:47 +0000

    Update common.c

--- common.c
+++ common.c
@@ -71,6 +71,23 @@ int initSocket() {
   }
 }
 
+int infraOut(int infraFrontL, int infraFrontR){
+  stopIf(!inAngle(infraFrontL && !inAngle(infraFrontR));
+	sprintf(buf, "I LR %i %i\n);
+	#ifdef DEBUG
+	printf("I LR %i %i\n",infraFrontL, infraFrontR);
+	#endif
+	nextCmd();
+}
+
+
+bool inAngle(int angleIR){
+     if (angleIR <=90 && angleIR >= -90)
+     return true;
+
+     return false;
+}
+
 bool inLimit(int voltage) {
   if (voltage <= 127 && voltage >= -127)
     return true;
